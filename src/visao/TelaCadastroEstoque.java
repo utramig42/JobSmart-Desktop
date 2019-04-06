7e2c4701231f96ca9b753d252b6469527ea5b1bf
Arthur Flávio de Abreu Tavares <277093@utramigbh.sede>
2019-04-06 09:08:14 -0300

    Tela etc

--- src/visao/TelaCadastroEstoque.java
+++ src/visao/TelaCadastroEstoque.java
@@ -9,12 +9,12 @@ package visao;
  *
  * @author 277017
  */
-public class CadastroEstoque extends javax.swing.JFrame {
+public class TelaCadastroEstoque extends javax.swing.JFrame {
 
     /**
      * Creates new form CadastroEstoque
      */
-    public CadastroEstoque() {
+    public TelaCadastroEstoque() {
         initComponents();
     }
 
@@ -244,21 +244,23 @@ public class CadastroEstoque extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CadastroEstoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaCadastroEstoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CadastroEstoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaCadastroEstoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CadastroEstoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaCadastroEstoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CadastroEstoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaCadastroEstoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new CadastroEstoque().setVisible(true);
+                new TelaCadastroEstoque().setVisible(true);
             }
         });
     }
